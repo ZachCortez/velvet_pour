45982005e952c4b0ce2bd06a2f42ce4fb032b668
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2025-06-01 23:49:12 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import Cocktails from './components/Cocktails.jsx'
+import About from './components/About.jsx'
+import Art from './components/Art.jsx'
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+    return (
+        <main>
+            <Navbar />
+            <Hero />
+            <Cocktails />
+            <About />
+            <Art />
+        </main>
+    )
+}
+
+export default App
