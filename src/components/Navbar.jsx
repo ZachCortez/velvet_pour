92f5210696a98c1379802026164de260cabef017
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2025-06-05 23:06:40 -0400

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react'
-
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
 import { navLinks } from '../../constants/index.js'
 
+gsap.registerPlugin(ScrollToPlugin);
+
+
+
 const Navbar = () => {
+    const handleScrollToTop = (e) => {
+        e.preventDefault(); // prevent default anchor behavior
+        gsap.to(window, {
+            scrollTo: { y: 0 },
+            duration: 1,
+            ease: 'power2.inOut',
+        });
+    };
+
+
     useGSAP(() => {
         const navTween = gsap.timeline({
             scrollTrigger: {
@@ -23,7 +37,7 @@ const Navbar = () => {
     return (
         <nav>
             <div>
-                <a href="#home" className="flex items-center gap-2">
+                <a href="#home" onClick={handleScrollToTop} className="flex items-center gap-2">
                     <img src="/images/logo.png" alt="logo" />
                     <p>Velvet Pour</p>
                 </a>
